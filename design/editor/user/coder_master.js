3f0a3e26647962038c91f5cfbd8c835bfeac2ffa
Basha <110662505+Bashamega@users.noreply.github.com>
2023-05-25 09:45:33 +0300

    design

---
+++ design/editor/user/coder_master.js
@@ -0,0 +1,14 @@
+const div_= document.createElement("div")
+div_.id="signature-coder"
+document.body.append(div_)
+const div = document.getElementById("signature-coder")
+div.innerHTML = "Designed by <a href='https://discord.gg/aJRKHRxG'>Coder_Master</a>"
+div.style = `
+    position: fixed;
+    top:97.5%;
+    left: 0;
+    background-color:darkgrey;
+    width:100%;
+
+`
+
